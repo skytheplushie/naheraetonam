354954038ad3f558434cfad22e5458267f3e726f
skytheplushie <ole16307@gmail.com>
2024-12-17 17:16:54 +0500

    Add files via upload

---
+++ idinahuiDjango/idinahuidjango/task1/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=9, decimal_places=2)
+    age = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=30)
+    cost = models.DecimalField(max_digits=9, decimal_places=2)
+    size = models.DecimalField(max_digits=9, decimal_places=2)
+    description = models.TextField()
+    age_limit = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games', default=None)
+
+    def __str__(self):
+        return self.title
+# Create your models here.
