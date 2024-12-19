7d5f3c629915e2c6d6efae12fe91e0679c6bb130
skytheplushie <ole16307@gmail.com>
2024-12-19 18:32:04 +0500

    Add files via upload

--- idinahuiDjango/idinahuidjango/task_one/admin.py
+++ idinahuiDjango/idinahuidjango/task_one/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Game, Buyer
 
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_filter = ('size', 'cost')
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title',)
+    list_per_page = 20
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_filter = ('balance', 'age')
+    list_display = ('name', 'balance ', 'age')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
 # Register your models here.
